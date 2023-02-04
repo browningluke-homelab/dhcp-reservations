5e900ca62f4b638eae4b63596992f764057ad0d6
Luke Browning <24992944+browningluke@users.noreply.github.com>
2023-02-03 21:06:28 -0500

    Add TF module (#2)
    * Add terraform module
    
    * Use path relative to module

---
+++ terraform/main.tf
@@ -0,0 +1,4 @@
+locals {
+  mac_list = yamldecode(file("${path.module}/../data_sources/reservations.yml"))
+  mac_addresses = {for each in local.mac_list.reservations : each.hostname => each.mac}
+}
---
+++ terraform/outputs.tf
@@ -0,0 +1,3 @@
+output "mac" {
+    value = local.mac_addresses[var.hostname]
+}
